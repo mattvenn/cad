730b307bfeb98ea8edc52546f8636bbb39a6cb16
Matt Venn <matt@mattvenn.net>
2012-06-07 23:15:19 +0100

    define length not width, better for laser cutters. changed stone size and width ratio. fixed 10cm line bug for stone file

--- go/go.py
+++ go/go.py
@@ -6,13 +6,14 @@ import argparse
 
 def makeStones(prop,name):
     d=Drawing(name)
-
+    lasty=0
     for x in range(prop['rows']):
         for y in range(prop['columns']):
             centre = ( prop['stoneMargin'] + prop['stoneRadius'] + x*(prop['stoneRadius']*2 + prop['laserSpacing']),prop['stoneRadius'] + y*(prop['stoneRadius']*2+prop['laserSpacing']))
+            lasty = centre[1]
             d.idCircle('cut',centre,prop['stoneRadius'])
    
-    draw100mmLine(d,0,(prop['columns']+1)*(prop['stoneRadius']*2) + 10)
+    draw100mmLine(d,0,lasty + prop['stoneRadius'] + 10)
 
     d.saveas()
 
@@ -59,35 +60,35 @@ def draw100mmLine(d,x,y):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="go board")
-    parser.add_argument('--boardwidth',
-        action='store', dest='boardWidth', type=float, default="280",
+    parser.add_argument('--boardHeight',
+        action='store', dest='boardHeight', type=float, default="280",
         help="board width in mm")
     parser.add_argument('--lines',
         action='store', dest='lines', type=int, default=19,
         help="number of lines on the board")
     parser.add_argument('--stoneCutLength',
-        action='store', dest='stoneCutLength', type=int, default=150,
+        action='store', dest='stoneCutLength', type=int, default=280,
         help="mm long side of material to cut the stones from")
     args = parser.parse_args()
 
     #set values
     prop = {}
-    prop['boardWidth'] = args.boardWidth
-    prop['widthHeightRatio'] = 1.1 #skew the board
+    prop['boardHeight'] = args.boardHeight
+    prop['widthHeightRatio'] = 0.95 #skew the board
     prop['lines'] = args.lines
     prop['laserSpacing'] = 2.0 ##from each other
-    prop['stoneSizeRatio'] = 0.8 #ratio to square width
+    prop['stoneSizeRatio'] = 0.9 #ratio to square width
     prop['stoneCutLength'] = args.stoneCutLength
     prop['markCentre'] = True
 
     #calculated values
-    prop['boardBorder'] = prop['boardWidth'] / 20
+    prop['boardBorder'] = prop['boardHeight'] / 20
     if prop['lines'] >= 13:
         prop['markCorners'] = True
     else:
         prop['markCorners'] = False
 
-    prop['boardHeight'] = prop['boardWidth'] * prop['widthHeightRatio']
+    prop['boardWidth'] = prop['boardHeight'] * prop['widthHeightRatio']
     prop['stoneRadius'] =  prop['stoneSizeRatio'] * prop['boardWidth'] / prop['lines'] / 2
     prop['stoneMargin'] = 0 # boardWidth + laserSpacing
     prop['lineWidth'] = prop['boardWidth'] - prop['boardBorder'] * 2
@@ -98,11 +99,11 @@ if __name__ == '__main__':
     numDots = prop['lines'] * prop['lines']
     numPieces = numDots * 0.75 #75% of the full number
     numPieces /= 2 #as we make 2 files, one for each colour
-    prop['rows'] = int(prop['stoneCutLength'] / (prop['stoneRadius'] * 2 + prop['laserSpacing'] ))
-    prop['columns'] = int(numPieces / prop['rows']) 
+    prop['columns'] = int(prop['stoneCutLength'] / (prop['stoneRadius'] * 2 + prop['laserSpacing'] ))
+    prop['rows'] = int(numPieces / prop['columns']) 
     actualNumPieces = prop['rows'] * prop['columns']
 
-    print "making a %d x %d board, %d mm wide, with %d stones of each colour" % ( prop['lines'], prop['lines'], prop['boardWidth'], actualNumPieces )
+    print "making a %d x %d board, (%d mm x %d mm), with %d stones of each colour" % ( prop['lines'], prop['lines'], prop['boardWidth'], prop['boardHeight'], actualNumPieces )
 
     makeBoard(prop,'board.svg')
     makeStones(prop,'stonesB.svg')
