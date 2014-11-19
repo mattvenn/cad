bc2aec2bae17ff7a15d84c9520cd84e0a517c506
matt venn <matt@mattvenn.net>
2014-11-19 11:41:17 +0000

    readme

---
+++ tools/harmonograph/README.md
@@ -0,0 +1,17 @@
+# Harmonograph
+
+A [harmonograph](http://en.wikipedia.org/wiki/Harmonograph) is a simple machine that draws amazing patterns. 
+
+This is an attempt to make a computer program that produces similiar patterns with 2 additional features:
+
+* allow an extra sine wave for modulating pen width
+* export the path as a GCODE file for engraving with a CNC machine
+
+# Requirements
+
+* Python
+* Tkinter (should be installed with your Python)
+* PIL library:
+    * Linux - sudo pip install PIL
+    * [Mac](http://stackoverflow.com/questions/9070074/how-to-install-pil-on-mac-os-x-10-7-2-lion)
+    * [Windows](http://stackoverflow.com/a/4579917)
--- tools/harmonograph/harmono.py
+++ tools/harmonograph/harmono.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python
-from PIL import Image, ImageTk
-from PIL import ImageDraw
+from PIL import Image, ImageTk, ImageDraw
 import math
-import ipdb
 import Tkinter
 import pickle
-import numpy as np
 
+scale_l = 150
 class Sine():
     #we store parent so we can call update() when a control is changed
     def __init__(self,number,parent):
@@ -21,12 +19,27 @@ class Sine():
 
         self.load()
 
+        self.frame = Tkinter.LabelFrame(text=self.number)
+
         self.controls = []
-        self.controls.append(Tkinter.Scale(label='a',from_=0, to=10, resolution=0.1, orient=Tkinter.VERTICAL,variable=self.amp,command=self.parent.update))
-        self.controls.append(Tkinter.Scale(label='f',from_=0, to=10, resolution=0.1, orient=Tkinter.VERTICAL,variable=self.freq,command=self.parent.update))
-        self.controls.append(Tkinter.Scale(label='p',from_=0, to=math.pi*2, resolution=0.005, orient=Tkinter.VERTICAL,variable=self.phase,command=self.parent.update))
-        self.controls.append(Tkinter.Scale(label='A',from_=0, to=1, resolution=0.01, orient=Tkinter.VERTICAL,variable=self.angle,command=self.parent.update))
-    
+
+        widget = Tkinter.Scale(self.frame,label='a',from_=0, to=300, resolution=1, orient=Tkinter.VERTICAL,variable=self.amp,length=scale_l,showvalue=0)
+        widget.bind('<ButtonRelease>',self.parent.update)
+        self.controls.append(widget)
+
+        widget = Tkinter.Scale(self.frame,label='f',from_=0, to=2, resolution=0.01, orient=Tkinter.VERTICAL,variable=self.freq,length=scale_l,showvalue=0)
+        widget.bind('<ButtonRelease>',self.parent.update)
+        self.controls.append(widget)
+
+        widget = Tkinter.Scale(self.frame,label='p',from_=0, to=math.pi*2, resolution=0.005, orient=Tkinter.VERTICAL,variable=self.phase,length=scale_l,showvalue=0)
+        widget.bind('<ButtonRelease>',self.parent.update)
+        self.controls.append(widget)
+
+        widget = Tkinter.Scale(self.frame,label='A',from_=0, to=1, resolution=0.01, orient=Tkinter.VERTICAL,variable=self.angle,length=scale_l,showvalue=0)
+        widget.bind('<ButtonRelease>',self.parent.update)
+       
+        self.controls.append(widget)
+
     def get_x(self,t):
         return self.amp.get() * math.sin(t*self.freq.get()+self.phase.get()) * self.angle.get() # math.sin(self.angle.get())
 
@@ -35,7 +48,9 @@ class Sine():
         return self.amp.get() * math.sin(t*self.freq.get()+self.phase.get()) * (1-self.angle.get())# math.cos(self.angle.get())
 
     def get_controls(self):
-        return self.controls
+        for control in self.controls:
+            control.pack(side=Tkinter.LEFT)
+        return [self.frame] #self.controls
     
     def save(self):
         config = {
@@ -68,19 +83,22 @@ class Frame():
 
     def __init__(self):
         self.tk = Tkinter.Tk()
-        self.size = (600,600)
+        self.size = (1000,700)
         #label is where we show the image
         self.bg = Tkinter.Label()
         self.bg.grid(row=1,column=0,columnspan=30,rowspan=30)
+        self.image = None
         self.new_background()
         self.update_image()
         
         self.length = Tkinter.IntVar()
-        length = Tkinter.Scale(label='L',from_=100, to=20000,resolution=100, orient=Tkinter.VERTICAL,variable=self.length,command=self.update)
+        length = Tkinter.Scale(label='points',from_=100, to=20000,resolution=100, orient=Tkinter.VERTICAL,variable=self.length,length=scale_l,showvalue=0)
+        length.bind('<ButtonRelease>',self.update)
         length.grid(row=0,column=0)
 
         self.res = Tkinter.DoubleVar()
-        res = Tkinter.Scale(label='R',from_=0.01, to=0.1, resolution=0.001, orient=Tkinter.VERTICAL,variable=self.res,command=self.update)
+        res = Tkinter.Scale(label='res',from_=0.01, to=0.1, resolution=0.001, orient=Tkinter.VERTICAL,variable=self.res,length=scale_l,showvalue=0)
+        res.bind('<ButtonRelease>',self.update)
         res.grid(row=0,column=1)
     
         #load defaults
@@ -93,20 +111,20 @@ class Frame():
         self.sines.append(Sine(3,self))
         self.sines.append(Sine(4,self))
 
+        self.zsine = Sine(5,self)
         #starting column for grid layout
         col = 2
-        for sine in self.sines:
+        for sine in self.sines + [self.zsine]:
             for c in sine.get_controls():
                 c.grid(row=0,column=col)
                 col += 1
-    
-        #do a first update
+
+        #do one update to start
         self.update()
     
     def update(self,*args):
+        #print("updated")
         self.new_background()
-        x = 0 #self.size[0]/2
-        y = 0 #self.size[1]/2
         min_y = 1000
         max_y = -1000
         min_x = 1000
@@ -115,12 +133,16 @@ class Frame():
 
         t = 0
         for step in range(self.length.get()):
+            x = 0 #self.size[0]/2
+            y = 0 #self.size[1]/2
             t = t + self.res.get()
             for sine in self.sines:
                 x += sine.get_x(t)
                 y += sine.get_y(t)
+                #hack for depth
+                z = abs(self.zsine.get_x(t))
 
-            points.append((x,y))
+            points.append((x,y,z))
 
             if x > max_x:
                 max_x = x
@@ -138,6 +160,7 @@ class Frame():
         y_off = height / 2 + self.size[1] / 2 - max_y
 
         """
+        print min_x,max_x
         print min_x,min_y
         print max_x,max_y
         print width, height
@@ -148,7 +171,7 @@ class Frame():
         last_point = None
         for point in points:
             if last_point:
-                self.draw.line([point[0]+x_off,point[1]+y_off,last_point[0]+x_off,last_point[1]+y_off],0)
+                self.draw.line([point[0]+x_off,point[1]+y_off,last_point[0]+x_off,last_point[1]+y_off],255,int(point[2]))
             last_point = point
         self.update_image()
         
@@ -157,7 +180,7 @@ class Frame():
 
     def save(self):
         #our sine waves 
-        for sine in self.sines:
+        for sine in self.sines + [self.zsine]:
             sine.save()
         #our controls
         config = {
@@ -180,13 +203,15 @@ class Frame():
             self.res.set(0.1)
     
     def new_background(self):
-        self.image = Image.new("L", self.size, "white")
+        self.image = Image.new("L", self.size, "black")
         self.draw = ImageDraw.Draw(self.image)
 
     def update_image(self):
         tk_image = ImageTk.PhotoImage(self.image)
         self.bg.configure(image = tk_image)
         self.bg.image = tk_image
+        #write it out
+        self.image.save("sine.png")
 
 
 frame = Frame()
