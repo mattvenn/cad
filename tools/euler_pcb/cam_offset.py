9e4f59111570527469046dcce24f2189f684ab71
Matt Venn <matt@mattvenn.net>
2015-04-10 14:59:17 +0100

    more comments and include the grecode command

--- tools/euler_pcb/cam_offset.py
+++ tools/euler_pcb/cam_offset.py
@@ -1,14 +1,25 @@
-x=+51.539
-y=-1.537
+#!/usr/bin/python
+import os
 
-x1=5
-y1=5
-x2=15
-y2=15
+#camera offsets
+x=+52.77
+y=-1.26
 
-a1=65.592
-b1=6.136
-a2=72.988
-b2=18.046
+#hole 1 in file
+#G00 X9.7488 Y5.7394 
+x1=9.7488 
+y1=5.7394 
+
+#hole 2 in file
+#G00 X68.0000 Y13.0800 
+x2=68
+y2=13.08
+
+#what they were with cam over holes
+a1=5.25
+b1=-11.85
+a2=61.98
+b2=2.72
 
 print("%f %f %f %f %f %f %f %f" % (x1,y1,x2,y2,a1-x,b1-y,a2-x,b2-y))
+os.system("cat button.ngc | ./grecode/grecode -overlay %f %f %f %f %f %f %f %f -o adjust.ngc " % (x1,y1,x2,y2,a1-x,b1-y,a2-x,b2-y))
