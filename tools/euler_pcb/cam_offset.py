98320e6f25bab01a155d9da0e40c216f203b69db
Matt Venn <matt@mattvenn.net>
2015-04-10 17:27:51 +0100

    file on command line

--- tools/euler_pcb/cam_offset.py
+++ tools/euler_pcb/cam_offset.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import argparse
 import os
 
 #camera offsets
@@ -21,5 +22,12 @@ b1=-11.85
 a2=61.98
 b2=2.72
 
+parser = argparse.ArgumentParser(description="use grecode to generate rotated/offset drill files")
+
+parser.add_argument('--gcode', default="drill.ngc", help='gcode file to read')
+args = parser.parse_args()
+
 print("%f %f %f %f %f %f %f %f" % (x1,y1,x2,y2,a1-x,b1-y,a2-x,b2-y))
-os.system("cat button.ngc | ./grecode/grecode -overlay %f %f %f %f %f %f %f %f -o adjust.ngc " % (x1,y1,x2,y2,a1-x,b1-y,a2-x,b2-y))
+os.system("cat %s | ./grecode/grecode -overlay %f %f %f %f %f %f %f %f -o adjust.ngc " % (args.gcode,x1,y1,x2,y2,a1-x,b1-y,a2-x,b2-y))
+
+
