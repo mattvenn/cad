9df1637a2d7ec3455e826f906ef39c4cbabe95c4
Matt Venn <matt@mattvenn.net>
2015-04-15 16:24:40 +0100

    smaller window

--- tools/euler_pcb/crosshair.py
+++ tools/euler_pcb/crosshair.py
@@ -9,18 +9,20 @@ w = 640
 cap.set(3,w)
 cap.set(4,h)
 
-#we draw on a blank background
+# we draw on a blank background
 cv2.namedWindow('background', cv2.CV_WINDOW_AUTOSIZE)
 
 while(1):
     # read the frames
     success,frame = cap.read()
     if success:
-
-        #draw a line that shows the slit
+        
+        # draw a line that shows the slit
         cv2.line(frame,(0,h/2),(w,h/2),255,1)
         cv2.line(frame,(w/2,0),(w/2,h),255,1)
-        cv2.imshow('background',frame)
+        # crop it
+        cropped = frame[h/4:3*h/4, w/4:3*w/4]
+        cv2.imshow('background',cropped)
 
     #if key pressed is 'Esc', exit the loop
     if cv2.waitKey(33)== 27:
