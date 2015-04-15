12050f30b7454fb407fcf541157abfa175e7e268
Matt Venn <matt@mattvenn.net>
2015-04-15 20:03:11 +0100

    defaults

--- tools/euler_pcb/cam_offset.py
+++ tools/euler_pcb/cam_offset.py
@@ -1,31 +1,38 @@
 #!/usr/bin/python
 import argparse
 import os
+import drill_extract as de
+import pickle
 
 #camera offsets
-x=+52.77
-y=-1.26
-
-#hole 1 in file
-#G00 X9.7488 Y5.7394 
-x1=9.7488 
-y1=5.7394 
-
-#hole 2 in file
-#G00 X68.0000 Y13.0800 
-x2=68
-y2=13.08
-
-#what they were with cam over holes
-a1=5.25
-b1=-11.85
-a2=61.98
-b2=2.72
+x=+50.93
+y=-3.821
+#x=+51.564
+#y=-0.749
 
 parser = argparse.ArgumentParser(description="use grecode to generate rotated/offset drill files")
 
-parser.add_argument('--gcode', default="drill.ngc", help='gcode file to read')
+parser.add_argument('--gcode', default="input.ngc", help='gcode file to read')
+parser.add_argument('--defaults', action='store_const', const=True, help='load last used')
 args = parser.parse_args()
+
+
+# load old defaults
+if args.defaults:
+    with open('offsets') as fh:
+        [x1,y1,x2,y2,a1,b1,a2,b2] = pickle.load(fh)
+else:
+    # holes in gcode
+    x1, y1 = de.get_gcode_xy(1)
+    x2, y2 = de.get_gcode_xy(2)
+
+    # what they were with cam over holes
+    a1, b1 = de.get_cam_xy(1)
+    a2, b2 = de.get_cam_xy(2)
+
+    # save them
+    with open('offsets','w') as fh:
+        pickle.dump([x1,y1,x2,y2,a1,b1,a2,b2], fh)
 
 print("%f %f %f %f %f %f %f %f" % (x1,y1,x2,y2,a1-x,b1-y,a2-x,b2-y))
 os.system("cat %s | ./grecode/grecode -overlay %f %f %f %f %f %f %f %f -o adjust.ngc " % (args.gcode,x1,y1,x2,y2,a1-x,b1-y,a2-x,b2-y))
